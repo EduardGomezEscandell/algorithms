1f94befa964cfd4168f1e98302f31f452839c131
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-07-09 17:59:04 +0200

    Minor change

--- src/karatsuba.cpp
+++ src/karatsuba.cpp
@@ -200,10 +200,10 @@ int main() {
     tests<unsigned int>();
     tests<unsigned long>();
 
+    std::cout << "Solving:" << std::endl;
     const my::big_uint<std::uint8_t> A {"3141592653589793238462643383279502884197169399375105820974944592"};
     const my::big_uint<std::uint8_t> B {"2718281828459045235360287471352662497757247093699959574966967627"};
 
-    std::cout << "Solving:" << '\n';
     std::cout << std::setw(110) << A <<'\n';
     std::cout << 'x' << std::setw(109) << B <<'\n';
     for(std::size_t i=0; i<110; ++i) std::cout << "-";
