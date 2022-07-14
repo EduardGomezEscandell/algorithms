eed961b9a1d6044194a8cc2c3e92c50bc9a66313
Edu Gómez Escandell <eduard.gomez.escandell@gmail.com>
2022-07-14 18:57:31 +0200

    Solved problem

--- demos/count_inversions.cpp
+++ demos/count_inversions.cpp
@@ -1,16 +1,29 @@
 #include "include/algorithms.hpp"
 
+#include <algorithm>
 #include <string_view>
 #include <vector>
 #include <iostream>
+#include <fstream>
 
-int main(int argc, char** argv)
+int main()
 {
-    std::vector<std::string_view> args(argv + 1, argv + argc);
+    std::vector<unsigned int> data;
+    data.reserve(100'000);
 
-    const auto inversion_count = my::sort_and_count_inversions(args.begin(), args.end());
-    std::cout << "Found " << inversion_count << " inversions." << std::endl;
-    std::cout << "Sorted: ";
-    std::for_each(args.cbegin(), args.cend(), [](auto& s) { std::cout << s << " "; });
-    std::cout << std::endl;
+    auto file = std::ifstream("data/numbers-1-to-100k-unsorted.txt");
+
+    if (file.bad()) {
+        std::cerr << "Failed to open file" << std::endl;
+        return 1;
+    }
+
+    while (!file.eof()) {
+        unsigned int num;
+        file >> num;
+        data.push_back(num);
+    }
+    std::cout << "Read " << data.size() << " values" << std::endl;
+    auto inversions = my::sort_and_count_inversions(data.begin(), data.end());
+    std::cout << "Found " << inversions << " inversions" << std::endl;
 }
